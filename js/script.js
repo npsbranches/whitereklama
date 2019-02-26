004821349c1a2273b18b4dab3997db6c3ee35b64
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-02-26 17:08:17 +0500

    1.1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('#callback-btn').click(function () {
+    $('.modal-form').toggleClass('visible');
+})
+
+$('.modal-form i').click(function () {
+    $('.modal-form').toggleClass('visible');
+})
+$('.modal-layout').click(function () {
+    $('.modal-form').toggleClass('visible');
+})
